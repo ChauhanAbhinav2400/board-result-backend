b0f12452003d980a726340530bf210e1bbf073bf
abhinav chauhan <abhinav@algodox.co.in>
2025-05-08 07:21:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.end("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
